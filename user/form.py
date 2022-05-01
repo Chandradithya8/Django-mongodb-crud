0dcc4ff70194c8b28e01ef6497bc4e356cb3d614
CHANDRADITHYA K.G <64347511+Chandradithya8@users.noreply.github.com>
2022-05-01 16:31:32 +0530

    Delete form.py

--- user/form.py
+++
@@ -1,7 +0,0 @@
-from django import forms
-from .models import User
- 
-class Add_A_User(forms.ModelForm):
-    class Meta:
-        model = User
-        fields = ['name', 'email', 'gender', 'phone_number']
